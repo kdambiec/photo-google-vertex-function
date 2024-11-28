2b92be2b436b6818ba167095c6b6eafd954b2016
Dambiec <karun.dambiec@capgemini.com>
2024-11-28 17:24:07 +0100

    Updated Settings

--- src/main/java/org/example/googlecloudfunctions/MyFunction.java
+++ src/main/java/org/example/googlecloudfunctions/MyFunction.java
@@ -2,6 +2,7 @@ package org.example.googlecloudfunctions;
 
 import com.google.cloud.vertexai.VertexAI;
 import com.google.cloud.vertexai.api.GenerateContentResponse;
+import com.google.cloud.vertexai.api.GenerationConfig;
 import com.google.cloud.vertexai.generativeai.ContentMaker;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
 import com.google.cloud.vertexai.generativeai.PartMaker;
@@ -13,15 +14,23 @@ public class MyFunction {
     private static final String LOCATION = "us-central1";
     private static final String IMAGE_URI = "gs://karuns-test-bucket/IMG_6116.JPEG";
 
-    public static void main(String[] args) throws Exception{
-        try (VertexAI vertexAi = new VertexAI(PROJECT_ID, LOCATION); ) {
+    public static void main(String[] args) throws Exception {
+        try (VertexAI vertexAi = new VertexAI(PROJECT_ID, LOCATION);) {
+            GenerationConfig generationConfig = GenerationConfig.newBuilder().
+                    setMaxOutputTokens(8192).
+                    setTemperature(1).
+                    setCandidateCount(1).
+                    build();
             // Vision model must be used for multi-modal input
-            GenerativeModel model = new GenerativeModel("gemini-1.5-pro-002", vertexAi);
-
+            GenerativeModel model = new GenerativeModel.Builder().
+                    setModelName("gemini-1.5-pro-002").
+                    setVertexAi(vertexAi).
+                    setGenerationConfig(generationConfig).
+                    build();
             GenerateContentResponse response =
                     model.generateContent(ContentMaker.fromMultiModalData(
-                            "Please create a caption describing the image",
-                            PartMaker.fromMimeTypeAndData("image/jpeg", IMAGE_URI)
+                            PartMaker.fromMimeTypeAndData("image/jpeg", IMAGE_URI),
+                            "Create a caption for the photo"
                     ));
             System.out.println(response);
             // Do something with the ResponseStream, which is an iterable.
